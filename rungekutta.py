4bf91829014a293298e5a0698d5b8b64e9862b65
Júlia Falcão <julia.falcao@accenture.com>
2019-05-23 10:00:03 -0300

    passo múltiplo funcionando

--- rungekutta.py
+++ rungekutta.py
@@ -10,47 +10,27 @@ def runge_kutta(h, y_seta, y_zero, intervalo):
         y[k+1] = y[k] + (h/2) * (y_seta(x[k], y[k]) + y_seta(x[k] + h, y[k]+h*y_seta(x[k], y[k])))
     return x, y
 
-    # y_um = y_zero + (h/2) * (y_seta(x_zero,y_zero) + y_seta(x_zero+h, y_zero+h*y_seta(x_zero,y_zero))
 
+def passo_multiplo(h, y_seta, y_zero, intervalo, inicializacao):
+    y = [y_zero]
+    x = list(np.arange(intervalo[0], intervalo[1], h))
+    
+    y.append(inicializacao(h, y_seta, y_zero, [intervalo[0], intervalo[0]+2*h])[1][1])    
+    
+    for k in range(1, len(x)-1):
+        y.append(None)
+        y[k+1] = y[k] + (h/2) * (3 * y_seta(x[k], y[k]) - y_seta(x[k-1], y[k-1]))
+    
+    return x, y
 
 def y_seta_ex(x, y):
     return -y+x-2
 
-def grafico(x, y):
-    vetor = []
-    for i in range(len(x)):
-        vetor.append([x[i], y[i]])
-
-    print(vetor)
-
-    fig, axs = plt.subplots(1, 1)
-    axs.plot(vetor)
-    plt.show()
-
-
-
-x, y = runge_kutta(0.5, y_seta_ex, 2, [0, 5])
-
+x1, y1 = runge_kutta(0.1, y_seta_ex, 2, [0, 5])
 fig, axs = plt.subplots(1, 1)
-axs.plot(x, y)
+axs.plot(x1, y1, color = "red")
 
-
-x, y = runge_kutta(0.4, y_seta_ex, 2, [0, 5])
-
-axs.plot(x, y)
-
-x, y = runge_kutta(0.3, y_seta_ex, 2, [0, 5])
-
-axs.plot(x, y)
-
-x, y = runge_kutta(0.2, y_seta_ex, 2, [0, 5])
-
-axs.plot(x, y)
-
-x, y = runge_kutta(0.1, y_seta_ex, 2, [0, 5])
-
-axs.plot(x, y)
+x2, y2 = passo_multiplo(0.1, y_seta_ex, 2, [0, 5], runge_kutta)
+axs.plot(x2, y2, color = "blue")
 
 plt.show()
-
-# grafico(x, y)
