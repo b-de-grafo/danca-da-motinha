f709f881ccdaf2904b5da48f1d38bbf8cfccc53e
rmparanhos <raffaelmp@id.uff.br>
2019-05-28 23:29:39 -0300

    n tenho ideia, n tenho vetor inicial

--- rungekutta.py
+++ rungekutta.py
@@ -69,7 +69,7 @@ def passo_multiplo(h, y_seta, y_zero, intervalo, inicializacao, n=1):
         y = [[y_zero[i] for i in range(n)]]  # [[1000, 300]]
         x = list(np.arange(intervalo[0], intervalo[1], h))
 
-        y.append(inicializacao(h, y_seta, y_zero, [intervalo[0], intervalo[0] + 2 * h], n=2)[1][1])
+        y.append(inicializacao(h, y_seta, y_zero, [intervalo[0], intervalo[0] + 2 * h], n=n)[1][1])
         for k in range(1,len(x) - 1):
             a = y_seta(x[k], y[k])
             b = y_seta(x[k-1], y[k-1])
@@ -79,6 +79,7 @@ def passo_multiplo(h, y_seta, y_zero, intervalo, inicializacao, n=1):
                 y[k + 1][i] = y[k][i] + h/2 * (3*a[i]-b[i])
         return x, y
 
+
 def y_seta_ex(x, y):
     return -y+x-2
 
@@ -87,12 +88,16 @@ def lotka_volterra(x, y, a=100, b=0.37, c=100, d=0.05):
     return (a*y[0] - b*y[0]*y[1], -c*y[1] + d*y[0]*y[1])
 
 
+def eq_motoca(x, y, k_1=21000, k_2=1500, m_1=40, m_2=400, b=1500, u=0):
+    return (y[1], (k_2 * (y[2] - y[0]) + b * (y[3] - y[1]) + k_1 * (u - y[0])) / m_1, y[3], (-k_2 * (y[2] - y[0]) - b * (y[3] - y[2])) / m_2)
+
+
 def transpoe_matriz(matriz):
     resp = [[matriz[j][i] for j in range(len(matriz))] for i in range(len(matriz[0]))]
     return resp
 
 
-fig, axs = plt.subplots(2, 1)
+
 
 """
 x1, y1 = runge_kutta(0.1, y_seta_ex, 2, [0, 5])
@@ -102,6 +107,8 @@ x2, y2 = passo_multiplo(0.1, y_seta_ex, 2, [0, 5], runge_kutta)
 axs.plot(x2, y2, color = "blue")
 """
 
+"""
+fig, axs = plt.subplots(2, 1)
 x, y_rk = runge_kutta(h=0.002, y_seta=lotka_volterra, y_zero=[1000, 300], intervalo=[0, 0.002*100], n=2)
 print(y_rk)
 x, y_pm = passo_multiplo(h=0.002, y_seta=lotka_volterra, y_zero=[1000, 300], intervalo=[0, 0.002*100], inicializacao=runge_kutta, n=2)
@@ -125,4 +132,30 @@ axs[1].plot(y_pm[0], y_pm[1], color="green")
 axs[1].set_ylabel("predador")
 axs[1].set_xlabel("presa")
 plt.show()
+"""
 
+fig, axs = plt.subplots(1, 1)
+x, y_rk = runge_kutta(h=0.01, y_seta=eq_motoca, y_zero=[0, 1, 0, 1], intervalo=[0, 10], n=4)
+print(x)
+print(y_rk)
+x, y_pm = passo_multiplo(h=0.01, y_seta=eq_motoca, y_zero=[0, 1, 0, 1], intervalo=[0, 10], inicializacao=runge_kutta, n=4)
+print(y_pm)
+
+y_rk = transpoe_matriz(y_rk)
+print(y_rk)
+
+y_pm = transpoe_matriz(y_pm)
+print(y_pm)
+
+axs.plot(x, y_rk[0], color="blue")
+axs.plot(x, y_rk[1], color="green")
+axs.plot(x, y_rk[2], color="red")
+axs.plot(x, y_rk[3], color="yellow")
+axs.plot(x, y_pm[0], color="blue")
+axs.plot(x, y_pm[1], color="green")
+axs.plot(x, y_pm[2], color="red")
+axs.plot(x, y_pm[3], color="yellow")
+axs.set_xlabel("tempo")
+
+
+plt.show()
