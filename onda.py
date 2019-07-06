237a09a05f1ccff54e5b4f2fc90390b8113dab2c
Wallace Baleroni <wallacebaleroni@gmail.com>
2019-07-06 18:40:26 -0300

    99% do trabalho 2 tá nesse commit

--- onda.py
+++ onda.py
@@ -37,7 +37,7 @@ def diferencas_finitas_exp(c_quadrado, delta_t, delta_x, intervalo, u_x_zero, co
             elif x == intervalo[1]:
                 u_i_k_mais_um = cond_contorno[1](0, t)
             else:
-                u_i_k_mais_um = gama_quadrado * (u[k][i-1] - 2*u[k][i] + u[k][i+1]) - u[k-1][i]
+                u_i_k_mais_um = gama_quadrado * (u[k][i-1] - 2*u[k][i] + u[k][i+1]) + 2 * u[k][i] - u[k-1][i]
             subu.append(u_i_k_mais_um)
             i = i + 1
         u.append(subu)
@@ -45,7 +45,7 @@ def diferencas_finitas_exp(c_quadrado, delta_t, delta_x, intervalo, u_x_zero, co
     return u
 
 
-def x_vezez_x_menos_1(x, t):
+def x_vezes_x_menos_1(x, t):
     return x*(x-1)
 
 
@@ -61,4 +61,4 @@ def dois(x, t):
     return 2
 
 
-print(diferencas_finitas_exp(4, 0.01, 0.2, [0, 1], x_vezez_x_menos_1, [sen_pi_vezes_t, zero], dois, 10))
+print(diferencas_finitas_exp(4, 0.01, 0.2, [0, 1], x_vezes_x_menos_1, [sen_pi_vezes_t, zero], dois, 10))
